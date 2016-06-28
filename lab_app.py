4494431c9974060d236e6a1834af8286f2f50fe4
joesnipes <joesnipes@gmail.com>
2016-06-28 00:07:29 -0400

    Update lab_app.py

--- lab_app.py
+++ lab_app.py
@@ -5,7 +5,7 @@ Version 1
 1. WHAT IT DOES
 Implements the first version of the project's Flask application.
 This version contains a single page that reports current temperature and humidity.
- 
+
 2. REQUIRES
 * Any Raspberry Pi
 3. ORIGINAL WORK
@@ -23,7 +23,7 @@ Libraries:
 from flask import Flask, request, render_template
 6. WARNING!
 None
-7. CREATED 
+7. CREATED
 8. TYPICAL OUTPUT
 A simple web page served by this flask application in the user's browser.
 The page contains the current temperature and humidity.
@@ -42,13 +42,15 @@ def hello():
 
 @app.route("/lab_temp")
 def lab_temp():
-	import sys
-	import Adafruit_DHT
-	humidity, temperature = Adafruit_DHT.read_retry(11 17)
-	if humidity is not None and temperature is not None:
-		return render_template("lab_temp.html",temp=temperature,hum=humidity)
-	else:
-		return render_template("no_sensor.html")
+        import sys
+        import Adafruit_DHT
+        humidity, temperature = Adafruit_DHT.read_retry(11, 17)
+        if humidity is not None and temperature is not None:
+                humidity = (humidity * 9/5) + 32
+                temperature = (temperature * 9/5) + 32
+                return render_template("lab_temp.html",temp=temperature,hum=humidity)
+        else:
+                return render_template("no_sensor.html")
 
 
 if __name__ == "__main__":
